3328c1aaac6090df31116f4931ab20bdb85ee369
douyun0623 <dyk6610@naver.com>
2025-06-05 19:16:32 +0900

    13주 2일

--- 2025 STL/2025 STL/메인.cpp
+++ 2025 STL/2025 STL/메인.cpp
@@ -1,29 +1,25 @@
 //---------------------------------------------------------------------------
-// 2025 STL 화56목78	 5월 29일 목요일							(13주 1일)
+// 2025 STL 화56목78	 6월 5일 목요일							(13주 2일)
 // 6월 19일 목요일 15주 2일 - 기말시험
 //---------------------------------------------------------------------------
-// Associative Container - key와 연관된 value를 항상 정렬상태로 유지한다.
-//						   정렬은 key값을 기준으로 한다.
-//	set	/ mutiset	- key == value
-//  map	/ multimap	- pair<key, value>
+// Unordered Associative Containers - hash 구조
 //---------------------------------------------------------------------------
 
 #include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <format>
+#include <unordered_set>	
 #include <print>
-#include <map>
 #include "save.h"
 #include "STRING.h"
 using namespace std;
 
 extern bool 관찰;					// 관찰하려면 true로 설정
 
-// 강의자료 "이상한 나라의 앨리스.txt"를 다운받는다.
-// [1] 여기에 있는 모든 단어를 multiset<STRING>에 저장하라.
-// [2] 모두 몇 단어인지 화면에 출력하라.	- 26626
-// [3] 단어와 사용횟수를 화면에 출력하라.
+template <>
+struct std::hash<STRING> {
+	size_t operator()(const STRING& s) const  {
+		return hash<std::string> {}(string{ s.begin(), s.end() });
+	}
+};
 
 //--------
 int main()
@@ -31,19 +27,26 @@ int main()
 {
 	save("메인.cpp");
 
-	ifstream in{ "이상한 나라의 앨리스.txt" };
-	if (not in) {
-		cout << "파일?" << endl;
-		return 20250527;
-	}
-
-	map<STRING, size_t> wordNum;
-
-	STRING 단어;
-	while (in >> 단어)
-		wordNum[단어]++;
+	unordered_set<STRING, hash<STRING>> us{ "1", "22", "333", "4444" };
 	
-	for (auto [단어, 개수] : wordNum)
-		cout << 단어 << " - " << 개수 << endl;
-		//println("{:20} - {:}", 단어[0], 개수);
+	for (const STRING& s : us)
+		cout << s << endl;
+
+	// 언오더드셋의 메모리를 화면에 출력한다.
+
+	while(true){
+		for (size_t bc = 0; bc < us.bucket_count(); ++bc) {
+			print("[{:3}] ", bc);
+			for (auto i = us.begin(bc); i != us.end(bc); ++i) {
+				print(" -> {:}", std::string(i->begin(), i->end()));
+			}
+
+			cout << endl;
+		}
+		cout << "추가할 STRING - ";
+		STRING s;
+		cin >> s;
+
+		us.insert(s);
+	}
 } 
